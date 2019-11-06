18930c5b81edff1ea4ddf848802d8d9a1006ca49
Andrew Kane <andrew@chartkick.com>
2019-11-05 22:31:56 -0800

    Fixed path on Windows

--- lib/libmf.rb
+++ lib/libmf.rb
@@ -11,17 +11,15 @@ module Libmf
   class << self
     attr_accessor :ffi_lib
   end
-  self.ffi_lib = ["mf"]
-
   lib_path =
     if ::FFI::Platform.windows?
-      "../vendor/windows/mf.dll"
+      "../vendor/libmf/windows/mf.dll"
     elsif ::FFI::Platform.mac?
       "libmf.bundle"
     else
       "libmf.so"
     end
-  self.ffi_lib << File.expand_path(lib_path, __dir__)
+  self.ffi_lib = [File.expand_path(lib_path, __dir__)]
 
   # friendlier error message
   autoload :FFI, "libmf/ffi"
