5acca4aee7d1187baad1d709e75db1b47e384c89
Andrew Kane <andrew@chartkick.com>
2020-03-09 04:50:30 -0700

    Use SSE

--- ext/libmf/extconf.rb
+++ ext/libmf/extconf.rb
@@ -5,21 +5,9 @@ case arch
 when /mingw/
   File.write("Makefile", dummy_makefile("libmf").join)
 else
-  abort "Missing stdc++" unless have_library("stdc++")
-  $CXXFLAGS << " -std=c++0x"
+  $CXXFLAGS << " -std=c++0x -DUSESSE"
 
-  # use SSE to accelerate LIBMF
-  # TODO use by default when available
-  if enable_config("sse")
-    $CXXFLAGS << " -DUSESSE"
-  end
-
-  # if have_library("omp")
-  #   # add -Xpreprocessor for Mac
-  #   $CXXFLAGS << " -fopenmp -DUSEOMP"
-  # end
-
-  $objs = ["mf.o"]
+  $srcs = ["mf.cpp"]
   vendor_path = File.expand_path("../../vendor/libmf", __dir__)
   create_makefile("libmf", vendor_path)
 end
